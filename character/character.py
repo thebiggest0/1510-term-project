93d48a8ed5a338a9a8ac174e29181fd1dd55287a
Cai Chun Yan <caichunyan17@gmail.com>
2023-11-24 21:33:23 -0800

    Complete creat_character function in character.py

---
+++ character/character.py
@@ -0,0 +1,26 @@
+"""
+ADD A DOCSTRING
+"""
+
+
+def creat_character(name):
+    character_info = {
+        'name': name,
+        'level': 1,
+        'hp': 50,
+        'str': 15,
+        'int': 15,
+        'dex': 15,
+        'x-coordinate': 0,
+        "y-coordinate": 0
+    }
+    return character_info
+
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
