fc39f28fdea2435017ec7c722cc86593f53a8b23
Cai Chun Yan <caichunyan17@gmail.com>
2023-11-25 14:03:39 -0800

    Implement functions for game_start.py

---
+++ introduction/game_start.py
@@ -0,0 +1,26 @@
+"""
+ADD A DOCSTRING
+"""
+
+
+def intro_text():
+    pass
+
+
+def name_entry():
+    pass
+
+
+def instructions():
+    pass
+
+def select_difficulty():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
