71bf16b1d74b1bad3ee597e81d343a3b89660d4f
Yifei Zeng <yzeng33@my.bcit.ca>
2023-11-25 15:22:51 -0800

    Complete function name_entry

--- introduction/game_start.py
+++ introduction/game_start.py
@@ -8,11 +8,20 @@ def intro_text():
 
 
 def name_entry():
-    pass
+    name = ''
+    while not name:
+        name = input('Please enter your player name: ')
+        if name:
+            final_confirm = input(f'Hello {name} please confirm your game name: \n"y" or "n" to complete entry: ')
+            if final_confirm == 'y':
+                return name
+            else:
+                name = ''
 
 
 def instructions():
     pass
+
 
 def select_difficulty():
     pass
