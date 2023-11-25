e4c8c19b9da49ec3255f97fcafbbff95a546ab04
Yifei Zeng <yzeng33@my.bcit.ca>
2023-11-25 15:32:36 -0800

    Complete docstring for function name_entry

--- introduction/game_start.py
+++ introduction/game_start.py
@@ -8,12 +8,20 @@ def intro_text():
 
 
 def name_entry():
+    """
+    Return user's name.
+
+    :precondition: function is called when the program requires the user's name for player identification
+    :postcondition: return user's name after successful entry and confirmation
+    :return: a string that user inputs
+    """
     name = ''
     while not name:
         name = input('Please enter your player name: ')
         if name:
-            final_confirm = input(f'Hello {name} please confirm your game name: \n"y" or "n" to complete entry: ')
-            if final_confirm == 'y':
+            final_confirm = input(f'Hello {name}, please confirm your game name: \n Enter "y" or "n" to '
+                                  f'complete entry: ')
+            if final_confirm.strip().lower() == 'y':
                 return name
             else:
                 name = ''
