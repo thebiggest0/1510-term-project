fb9749f7b8c758927c1394565da8085599ccb21c
Cai Chun Yan <caichunyan17@gmail.com>
2023-11-29 21:58:35 -0800

    Complete random_sum_generator in combat.py

--- character_actions/combat.py
+++ character_actions/combat.py
@@ -2,6 +2,21 @@
 ADD A DOCSTRING
 """
 
+import random
+
+
+def random_sum_generator():
+    number_one = random.randrange(0, 500)
+    number_two = random.randrange(-100, 300)
+    dice = random.randrange(0, 6)
+    if dice % 2 == 0:
+        random_result = number_one + number_two
+        operation = '+'
+    else:
+        random_result = number_one - number_two
+        operation = '-'
+    return [f'({number_one} {operation} {number_two} = ? )', random_result]
+
 
 def run_away():
     dice = random.randrange(0, 6)
@@ -13,6 +28,7 @@ def run_away():
 
 
 def main():
+    pass
 
 
 if __name__ == "__main__":
