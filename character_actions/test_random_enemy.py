3e20fdc9720126128661e5eccfa8c079a3cbfe08
Cai Chun Yan <caichunyan17@gmail.com>
2023-12-01 23:01:58 -0800

    Update unit test for random_enemy function

--- character_actions/test_random_enemy.py
+++ character_actions/test_random_enemy.py
@@ -4,10 +4,29 @@ from combat import random_enemy
 
 
 class TestRandomEnemy(TestCase):
-    @patch('combat.save_data.read_enemy')
-    @patch('combat.random.randrange')
-    def test_random_enemy_level_4(self, mock_randrange, mock_read_enemy):
-        pass
+
+    @patch('random.randrange', side_effect=[4, 3])
+    def test_random_enemy_level_4(self, _):
+        expected = {
+                    'name': 'Sith Troopers',
+                    'difficulty': 2,
+                    'hp': 50,
+                    'str': 10,
+                    'experience': 5}
+        actual = random_enemy()
+        self.assertEqual(expected, actual)
+
+    @patch('random.randrange', side_effect=[3, 0])
+    def test_random_enemy_level_not_4(self, _):
+        expected = {
+                    "name": "Clone Trooper",
+                    "difficulty": 1,
+                    "hp": 20,
+                    "str": 2,
+                    "experience": 2
+                }
+        actual = random_enemy()
+        self.assertEqual(expected, actual)
 
     @patch('combat.save_data.read_enemy')
     @patch('combat.random.randrange')
@@ -23,4 +42,3 @@ class TestRandomEnemy(TestCase):
 
         self.assertIn(result['name'], ['Clone Trooper', 'Battle Droids', 'Tusken Raiders'])
         self.assertEqual(set(result.keys()), {'name', 'health', 'attack', 'defense'})
-
