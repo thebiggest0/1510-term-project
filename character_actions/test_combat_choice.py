78338d0f16835e0ff9143b1120a215fd89bbf7b1
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-01 20:00:00 -0800

    Complete unit tests for function combat_choice

---
+++ character_actions/test_combat_choice.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from unittest.mock import patch
+from combat import combat_choice
+
+
+class TestCombatChoice(TestCase):
+    @patch('builtins.input', side_effect=['1'])
+    def test_user_choice_1(self, _):
+        actual = combat_choice()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.input', side_effect=['2'])
+    def test_user_choice_2(self, _):
+        actual = combat_choice()
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.input', side_effect=['3'])
+    def test_user_choice_3(self, _):
+        actual = combat_choice()
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.input', side_effect=['?', '1'])
+    def test_one_invalid_input(self, _):
+        actual = combat_choice()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.input', side_effect=['?', '2.3', '1'])
+    def test_many_invalid_input(self, _):
+        actual = combat_choice()
+        expected = 1
+        self.assertEqual(actual, expected)
