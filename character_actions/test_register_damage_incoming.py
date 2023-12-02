6f64e14a5f282cd9ecc3ba09365bde31efafcb3b
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-01 22:17:19 -0800

    Complete unit tests for function register_damage_incoming

---
+++ character_actions/test_register_damage_incoming.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+from combat import register_damage_incoming
+
+
+class Test(TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_register_damage_incoming_damage_taken(self, mock_stdout):
+        character = {'name': 'Luke Skywalker', 'hp': 5}
+        enemy = {'name': 'Royal Guard', 'str': 2}
+        result = register_damage_incoming(character, enemy)
+        self.assertTrue(result)
+        self.assertEqual(mock_stdout.getvalue().strip(), 'You took 2 damage')
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_register_damage_incoming_character_died(self, mock_stdout):
+        character = {'name': 'Luke Skywalker', 'hp': 2}
+        enemy = {'name': 'Royal Guard', 'str': 50}
+        result = register_damage_incoming(character, enemy)
+        self.assertFalse(result)
+        self.assertEqual(mock_stdout.getvalue().strip(), 'Your HP has fallen to 0, you have died...')
