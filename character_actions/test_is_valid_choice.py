4db0116264f907382c03824e22c01f8847f0bdfa
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-01 19:51:01 -0800

    Complete unit tests for function combat_choice

---
+++ character_actions/test_is_valid_choice.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from combat import is_valid_choice
+
+
+class TestValidChoice(TestCase):
+    def test_valid_choice_1(self):
+        actual = is_valid_choice('1')
+        self.assertTrue(actual)
+
+    def test_valid_choice_2(self):
+        actual = is_valid_choice('2')
+        self.assertTrue(actual)
+
+    def test_valid_choice_3(self):
+        actual = is_valid_choice('3')
+        self.assertTrue(actual)
+
+    def test_invalid_choice_float(self):
+        actual = is_valid_choice('1.222')
+        self.assertFalse(actual)
+
+    def test_invalid_choice_string(self):
+        actual = is_valid_choice('uuu')
+        self.assertFalse(actual)
