b5bc37139ad5c7c9afe7a2ec8fa86a1d56196ab0
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-07 13:22:19 -0800

    Modify unit tests for function random_enemy

--- character_actions/test_random_enemy.py
+++ character_actions/test_random_enemy.py
@@ -27,18 +27,3 @@ class TestRandomEnemy(TestCase):
                 }
         actual = random_enemy()
         self.assertEqual(expected, actual)
-
-    @patch('combat.save_data.read_enemy')
-    @patch('combat.random.randrange')
-    def test_random_enemy_level_other(self, mock_randrange, mock_read_enemy):
-        mock_randrange.return_value = 2
-        mock_read_enemy.return_value = {
-            'Clone Trooper': {'name': 'Clone Trooper', 'health': 100, 'attack': 20, 'defense': 10},
-            'Battle Droids': {'name': 'Battle Droids', 'health': 80, 'attack': 15, 'defense': 8},
-            'Tusken Raiders': {'name': 'Tusken Raiders', 'health': 90, 'attack': 18, 'defense': 12}
-        }
-
-        result = random_enemy()
-
-        self.assertIn(result['name'], ['Clone Trooper', 'Battle Droids', 'Tusken Raiders'])
-        self.assertEqual(set(result.keys()), {'name', 'health', 'attack', 'defense'})
