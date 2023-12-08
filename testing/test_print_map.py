c2bb2f2f2d4d8f9fd0a01ed5acb7d5962199d0ed
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-07 20:27:42 -0800

    Modify unit test for function print_map

--- testing/test_print_map.py
+++ testing/test_print_map.py
@@ -1,52 +1,22 @@
 from unittest import TestCase
 from unittest.mock import patch
-import io
+from io import StringIO
 from character_actions.move import print_map
 
 
 class TestPrintMap(TestCase):
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_small_map_with_player(self, mock_output):
-        game_map = [['#', '_', '#'], ['_', 'X', '_'], ['#', '_', '#']]
-        player_position = [1, 1]
-
-        print_map(game_map, player_position)
-        the_game_printed_this = mock_output.getvalue()
-
-        expected_output = (
-            '| # _ # |\n'
-            '| _ X _ |\n'
-            '| # _ # |\n'
-        )
-        self.assertEqual(expected_output, the_game_printed_this)
-
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_with_player_large_map(self, mock_output):
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_print_map_with_player_position(self, mock_stdout):
         game_map = [
-            ['_', '_', '_', '_', '_', '_', '#', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', 'O', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-            ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+            ["J", "O"],
+            ["#", "#"],
+            [" ", "P"]
         ]
-        player_position = [6, 0]
+        player_position = [2, 1]
+        expected_output = """_  _  _  _
+|  J  O  |
+|  #  #  |
+|     P  |
+-  -  -  -\n"""
         print_map(game_map, player_position)
-        the_function_printed_this = mock_output.getvalue()
-        expected_output = (
-            '| _ _ _ _ _ _ X _ _ _ |\n'
-            '| _ _ _ _ _ _ _ O _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-            '| _ _ _ _ _ _ _ _ _ _ |\n'
-        )
-        self.assertEqual(expected_output, the_function_printed_this)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
