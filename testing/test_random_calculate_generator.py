7031be7f173e4086b0482b528626c3023b8f89b5
Yifei Zeng <yzeng33@my.bcit.ca>
2023-12-09 23:11:04 -0800

    Modify function random_calculate_generator

--- testing/test_random_calculate_generator.py
+++ testing/test_random_calculate_generator.py
@@ -1,15 +1,16 @@
 from unittest import TestCase
 from unittest.mock import patch
 from character_actions.combat import random_calculate_generator
+import io
 
 
 class TestRandomCalculate(TestCase):
     @patch('random.randrange', side_effect=[23, 8, 2])
     def test_random_sum_generator_addition(self, _):
         result = random_calculate_generator()
-        self.assertEqual(result, ['23 + 8 = ', 31])
+        self.assertEqual(result, ['23 + (8) = ', 31])
 
     @patch('random.randrange', side_effect=[42, 15, 1])
     def test_random_sum_generator_subtraction(self, _):
         result = random_calculate_generator()
-        self.assertEqual(result, ['42 - 15 = ', 27])
+        self.assertEqual(result, ['42 - (15) = ', 27])
